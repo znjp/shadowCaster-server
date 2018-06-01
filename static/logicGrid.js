c26acc9d2202064c910c373db2bfdd2010ef869c
meinfr13nd <sarahsamora@mail.com>
2018-05-31 21:59:23 -0700

    displaying the timer is non-functional, but been working on it for a bit

--- static/logicGrid.js
+++ static/logicGrid.js
@@ -1,28 +1,78 @@
-boardSetup = [null, null, null, null, null, null, null, null, null, null, null, null,null, null, null, null];
+function timer(callback, delay) {
+    var id, started, remaining = delay, running
 
-$("td").click(function() {
-  var currentClasses = ($(this).attr("class")).split(' ');
-  if (currentClasses.length > 1) {
-  	var row = Number(currentClasses[1][3]);
-  	var col = Number(currentClasses[0][3]);
-  	if (boardSetup[row*4 + col]) {
-		boardSetup[row*4 + col] = null
-		truthCorrect(col, row);
-  	} else if (boardSetup[row*4 + col] == null) {
-		boardSetup[row*4 + col] = true
-  	} else if (boardSetup[row*4 + col] === false) {
-		boardSetup[row*4 + col] = true
-        	truthCorrect(col, row);
-  	}
-  	cleanBoard();
-  	if (clrBoard() == 4) {
-      if (checkRes()) {
-        document.location = "/release?e=c03144f689169c6f829f920613285ca58a4ab9bd";
-      } else {
-
-      }
+    this.start = function() {
+        running = true
+        started = new Date()
+        id = setTimeout(callback, remaining)
     }
-  }
+
+    this.pause = function() {
+        running = false
+        clearTimeout(id)
+        remaining -= new Date() - started
+    }
+
+    this.getTimeLeft = function() {
+        if (running) {
+            this.pause()
+            this.start()
+        }
+
+        return remaining
+    }
+
+    this.getStateRunning = function() {
+        return running
+    }
+
+    this.start()
+}
+
+boardSetup = [null, null, null, null, null, null, null, null, null, null, null, null,null, null, null, null];
+losing = true
+$("td").click(function() {
+	console.log(losing)
+	if (losing === true) {
+  		var currentClasses = ($(this).attr("class")).split(' ');
+  		if (currentClasses.length > 1) {
+  			var row = Number(currentClasses[1][3]);
+  			var col = Number(currentClasses[0][3]);
+  			if (boardSetup[row*4 + col]) {
+				boardSetup[row*4 + col] = null
+				truthCorrect(col, row);
+  			} else if (boardSetup[row*4 + col] == null) {
+				boardSetup[row*4 + col] = true
+  			} else if (boardSetup[row*4 + col] === false) {
+				boardSetup[row*4 + col] = true
+        			truthCorrect(col, row);
+  			}	
+  			cleanBoard();
+  			if (clrBoard() == 4) {
+      				if (checkRes()) {
+        				document.location = "/release?e=c03144f689169c6f829f920613285ca58a4ab9bd";
+      				} else {
+					losing = false;
+					alert("Incorrect deduction.\r\nYou must pause to let the shadowcasters pass.");
+					punish = new timer(function() {
+						losing = true;
+        					console.log('3 ' + losing);					 
+					}, 4000)
+					$('#timing').removeClass('hidden');
+					//document.getElementById("timing").outerHTML = punish.getTimeLeft()/1000;
+					while (punish.getTimeLeft()>0) {
+						//$('#timing').text(punish.getTimeLeft()/1000);
+						//console.log(punish.getTimeLeft()/1000);
+						//console.log(document.getElementById("timing"))
+						document.getElementById("timing").innerHTML = punish.getTimeLeft()/1000;
+						//console.log(document.getElementById("hidden").outerHTML)
+						//console.log(document.getElementById("timing").outerHTML)
+					}
+					$('#timing').addClass('hidden');
+      				}
+    			}
+  		}
+	}	
 });
 
 function cleanBoard() {
